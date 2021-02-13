e5ae2c7c24f469e76fb6c80186eb51b8f073521a
CharlesRinaldini <78990924+CharlesRinaldini@users.noreply.github.com>
2021-02-12 19:01:59 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#from unittests import test_dimSubsidiaryJDE
+from unittests import test_simple
+
+def main():
+    #test_dimSubsidiaryJDE.test_dimSubsidiaryJDE_transform_differences()
+    test_simple.test_simple_transform_differences()
+
+if __name__ == '__main__':
+    main()
+
